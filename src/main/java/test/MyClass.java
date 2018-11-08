68311caa1a9acbed5c02cc92369e38bb3b2abf4f
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2018-11-07 17:25:47 -0800

    Added myclass with gradle 5.0

--- src/main/java/test/MyClass.java
+++ src/main/java/test/MyClass.java
@@ -2,11 +2,9 @@ package test;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class MyClass {
     private Long id;
 }
